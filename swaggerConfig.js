4688d4dac638fbe9734a80a1fc11fa9513ca5b05
Daniel Fijalka <aklajifd@gmail.com>
2026-07-22 11:44:37 -0500

    updated API name in README and REST docs

--- swaggerConfig.js
+++ swaggerConfig.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3200/museumapi",
+        url: "http://localhost:3200/sprintboardapi",
         description: "Local development server",
       },
     ],
